caeb5b7724c3453fd53e84f00473c0c4b89feb2a
ahmetyasin <200207028@kocaeli.edu.tr>
2022-10-09 18:32:52 +0300

    update...

---
+++ app.py
@@ -0,0 +1,63 @@
+import telebot
+from flask import Flask, request
+from binance.client import Client
+from binance.enums import *
+import json, config
+
+
+app = Flask(__name__)
+
+
+client = Client(config.api_key, config.api_secret)
+
+#binance order fonksiyonu
+def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
+    try:
+        print(f"sending order {order_type} - {side} {quantity} {symbol}")
+        order = client.create_order(
+            symbol=symbol, side=side, type=order_type, quantity=quantity
+        )
+    except Exception as e:
+        print("an exception occured - {}".format(e))
+        return False
+
+    return order
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+
+    #json dosya tipinin okunmasını 
+    data = json.loads(request.data)
+    side = data["startegy"]["order_action"].upper()
+    quantity = data["startegy"]["order_contracts"]
+    ticker = data["ticker"]
+
+    #telegrama sinyal hakkında mesaj göndermek için
+    telebot.TeleBot(config.telegramBotApi).send_message(
+        config.telegramUserId,
+        f"""{data["time"]}
+                                                                         {data["exchange"]}:{data["ticker"]}
+                                                                         FİYAT = {data}
+                                                                         ALARM TİPİ = {data["strategy"]["order_price"]}
+                                                                        """,
+    )
+
+    #binance emir verme 
+    order_response = order(side, quantity, ticker)
+    print(order_response)
+
+    if order_response:
+        return {
+                "code": "success"
+                }
+    else:
+        print("order failed")
+        return {
+                "code": "error", 
+                "message": "order failed "
+                }
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ config.py
@@ -0,0 +1,7 @@
+#telegram
+telegramBotApi = "yourtelegramBotapi"
+telegramUserId = "yourtelegramUserId"
+
+#binance
+api_key = "your_binance_api_key"
+api_secret = "your_binance_api_secret"
